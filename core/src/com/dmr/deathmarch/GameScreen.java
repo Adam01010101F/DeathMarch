3c0cd5c396009752daaaa47cb9000a1c2ff1405c
Adam01010101F <watsaname1999@gmail.com>
2018-10-01 08:39:24 -0700

    Added player 1 along with simple keybindings.

--- core/src/com/dmr/deathmarch/GameScreen.java
+++ core/src/com/dmr/deathmarch/GameScreen.java
@@ -1,20 +1,36 @@
 package com.dmr.deathmarch;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
 
 public class GameScreen implements Screen {
     final DeathMarch game;
-    private Texture p1Tex;
-    private Texture p2Tex;
-    private Rectangle p1;
-    private Rectangle p2;
+    private OrthographicCamera camera;
+    private Texture pOneTex;
+    private Texture pTwoTex;
+    private Rectangle pOne;
+    private Rectangle pTwo;
     private Sound useWeapon;
 
     public GameScreen(final DeathMarch game){
         this.game = game;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 1280, 720);
+
+        pOneTex = new Texture(Gdx.files.internal("player1.png"));
+        pOne = new Rectangle();
+        pOne.x = 720/2 - 64/2;
+        pOne.y = 1280/2;
+        pOne.width = 120;
+        pOne.height = 120;
+
 
     }
     @Override
@@ -24,7 +40,29 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0,0, 0, 0.3f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+        game.batch.begin();
+        game.batch.draw(pOneTex, pOne.x, pOne.y);
+        game.batch.end();
+
+        //Player 1 Keybindings
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            pOne.x += 350 * Gdx.graphics.getDeltaTime();
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            pOne.x -= 350 * Gdx.graphics.getDeltaTime();
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+            pOne.y += 350 * Gdx.graphics.getDeltaTime();
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            pOne.y -= 350 *Gdx.graphics.getDeltaTime();
+        }
+        //boundaries
     }
 
     @Override
