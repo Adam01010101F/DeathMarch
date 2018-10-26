f3e0486297d5bf0b2a5d06382e01c57201b3a41d
Raymond Chin <iraychin@gmail.com>
2018-10-26 15:18:22 -0700

    Changed shop to different map
    Players will be transported to new map to purchase items

--- core/src/com/dmr/deathmarch/shopScreen.java
+++ core/src/com/dmr/deathmarch/shopScreen.java
@@ -1,25 +1,109 @@
 package com.dmr.deathmarch;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+//import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.dmr.deathmarch.npc.Goblin;
+import com.dmr.deathmarch.weapons.BeamCannon;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class shopScreen implements Screen{
     final DeathMarch game;
     public OrthographicCamera camera;
     public Stage stage;
+    private Texture pOneTex;
+    private Texture pTwoTex;
+    private Texture bmTex;
+    private Texture lbTex;
+    private Player pOne;
+    private Player pTwo;
+    private LogicModel lm;
+    private Direction lastDirection[];
+    AssetManager assetManager;
+    TiledMap tiledMap;
+    TiledMapRenderer tiledMapRenderer;
+    private Sprite beamProjectile;
+    private Sprite heart;
+    private Sprite bullet;
+    private Texture heartTex;
+    private Texture bulletTex;
+    private boolean gamePaused;
+    private Sprite door;
+    private Texture doorTex;
+
+    private OrthogonalTiledMapRenderer renderer;
+    private TiledMap map;
+
+
+    private Skin skin;
+
+    private Dialog dialog;
 
     public shopScreen(final DeathMarch game){
         this.game=game;
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1280, 720);
 
+
+
+        Skin skin = new Skin(Gdx.files.internal("skin/pixthulhu-ui.json"));
+        pOneTex = new Texture(Gdx.files.internal("player1.png"));
+        pTwoTex = new Texture(Gdx.files.internal("player2.png"));
         stage = new Stage(new ScreenViewport());
+
+        //Player Creation
+        pOne = new Player("Shredder", false, pOneTex);
+        pOne.setPosition(1280/2f - 120/2f, 720/2f);
+        pTwo = new Player("Donatello", false, pTwoTex);
+        pTwo.setPosition((1280/2f -120/2f), 720/2f);
+
+        //npc
+        heartTex = new Texture(Gdx.files.internal("heart.png"));
+        heart = new Sprite(heartTex);
+
+        bulletTex = new Texture(Gdx.files.internal("Vessel.png"));
+        bullet = new Sprite(bulletTex);
+        bullet.setScale(5f);
+
+        doorTex = new Texture(Gdx.files.internal("Stairs_down.png"));
+        door = new Sprite(doorTex);
+        //npc
+        bullet.setX(550);
+        bullet.setY(73);
+
+        heart.setScale(8f);
+        heart.setX(350);
+        heart.setY(73);
+
+        // 5 & 12
+        door.setScale(8f);
+        door.setX(22);
+        door.setY(20);
     }
     @Override
     public void render(float delta){
@@ -40,10 +124,99 @@ public class shopScreen implements Screen{
 //            dispose();
 //        }
 
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.clear();
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        Gdx.input.setInputProcessor(stage);
         stage.draw();
+        Gdx.gl.glClearColor(0,0.3f, 0, 0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+
+        camera.update();
+
+        game.batch.setProjectionMatrix(camera.combined);
+
+        // Load Objects onto Screen
+        game.batch.begin();
+        game.font.draw(game.batch, "P2 x: "+pTwo.getX()+" y: "+pTwo.getY(), 100, 150);
+        pOne.draw(game.batch);
+        pTwo.draw(game.batch);
+
+        heart.draw(game.batch);
+        bullet.draw(game.batch);
+        door.draw(game.batch);
+        game.batch.end();
+
+        //Player 1 Keybindings
+        if(Gdx.input.isKeyPressed(Input.Keys.W) || Gdx.input.isKeyPressed(Input.Keys.D) ||
+                Gdx.input.isKeyPressed(Input.Keys.A)|| Gdx.input.isKeyPressed(Input.Keys.S)){
+            pOne.clearDirections();
+//            int condRot = (pOne.getWeapon().getRotation()/90)%2==0 ? -90: 90;
+            if(Gdx.input.isKeyPressed(Input.Keys.W)){
+                pOne.setRotation(90);
+                pOne.setY(pOne.getY()+150*Gdx.graphics.getDeltaTime());
+                pOne.setYDirection(Direction.Up);
+
+            }
+            if(Gdx.input.isKeyPressed(Input.Keys.S)){
+                pOne.setRotation(270);
+                pOne.setY(pOne.getY()-150*Gdx.graphics.getDeltaTime());
+                pOne.setYDirection(Direction.Down);
+
+
+
+            }
+            if(Gdx.input.isKeyPressed(Input.Keys.D)){
+                pOne.setRotation(0);
+                pOne.setX(pOne.getX()+150*Gdx.graphics.getDeltaTime());
+                pOne.setXDirection(Direction.Right);
+
+
+            }
+            if(Gdx.input.isKeyPressed(Input.Keys.A)){
+                pOne.setRotation(180);
+                pOne.setX(pOne.getX()-150*Gdx.graphics.getDeltaTime());
+                pOne.setXDirection(Direction.Left);
+
+            }
+
+        }
+
+        //Player 2 Keybindings
+        if(Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.RIGHT) ||
+                Gdx.input.isKeyPressed(Input.Keys.LEFT)|| Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            pTwo.clearDirections();
+            if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+                pTwo.setRotation(90);
+                pTwo.setY(pTwo.getY() + 150 * Gdx.graphics.getDeltaTime());
+                pTwo.setYDirection(Direction.Up);
+            }
+            if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+                pTwo.setRotation(270);
+                pTwo.setY(pTwo.getY() - 150 * Gdx.graphics.getDeltaTime());
+                pTwo.setYDirection(Direction.Down);
+            }
+            if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+                pTwo.setRotation(0);
+                pTwo.setX(pTwo.getX() + 150 * Gdx.graphics.getDeltaTime());
+                pTwo.setXDirection(Direction.Right);
+            }
+            if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+                pTwo.setRotation(180);
+                pTwo.setX(pTwo.getX() - 150 * Gdx.graphics.getDeltaTime());
+                pTwo.setXDirection(Direction.Left);
+            }
+        }
+        //leaving shop area
+        if (Gdx.input.isKeyPressed(Input.Keys.P) && pOne.getBoundingRectangle().overlaps(door.getBoundingRectangle()) && pTwo.getBoundingRectangle().overlaps(door.getBoundingRectangle())) {
+            game.changeScreen(DeathMarch.APPLICATION);
+
+        }
+
+        //Player Boundaries
+        checkBoundary(pOne);
+        checkBoundary(pTwo);
 
 
     }
@@ -75,121 +248,16 @@ public class shopScreen implements Screen{
 
     @Override
     public void show(){
-        //table
-        Skin shopSkin = new Skin(Gdx.files.internal("skin/pixthulhu-ui.json"));
-
-        //window
-           /* Window window = new Window("ShopKeeper", shopSkin );
-            window.setPosition(400,200);
-            window.pack();*/
-
-        //table
-        stage.clear();
-        Gdx.input.setInputProcessor(stage);
-
-        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1/30f));
-        stage.draw();
-
-        //constrains table size
-        Container<Table> tableContainer = new Container<Table>();
-        float sw = Gdx.graphics.getWidth();
-        float sh = Gdx.graphics.getHeight();
-
-        float cw = sw * 0.7f;
-        float ch = sh * 0.5f;
-
-        //creates container restraints
-        tableContainer.setSize(cw, ch);
-        tableContainer.setPosition((sw - cw) / 2.0f, (sh - ch) / 2.0f);
-        tableContainer.fillX();
-
-        //creates table
-        Table table = new Table();
-        table.setFillParent(true);
-        table.setDebug(true);
-        stage.addActor(table);
-
-        //creates subTable so that exit button is center
-        Table subTable = new Table();
-        table.setFillParent(true);
-        table.setDebug(true);
-        stage.addActor(subTable);
-
-
-        //sets size of table
-        table.setSize(800,480);
-        subTable.setSize(800,480);
-
-        //label for table includes items and title
-        Label topLabel = new Label("Shop Keeper",shopSkin);
-        Label grenades = new Label("Grenades",shopSkin);
-        Label health = new Label("Health Booster",shopSkin);
-        //buttons to buy itesm
-        TextButton buyg = new TextButton("Buy",shopSkin);
-        TextButton buyh = new TextButton("Buy",shopSkin);
-        //button to exit
-        TextButton buttonExit = new TextButton("Exit" , shopSkin);
-        table.add(buttonExit);
-        //adds the rows for Title Label
-        table.row().colspan(2).expandX().fillX();
-        table.add(topLabel).fillX();
-        table.row().colspan(2).expandX().fillX();
-        //adds the rows for grenades
-        table.row().colspan(2).expandX().fillX();
-        table.add(grenades).expandX().fillX();
-        //the following commented changes size of button.
-        //table.add(buyg).width(Value.percentWidth(.25F,table));
-        table.add(buyg).expandX().fillX();
-        //adds the rows for health booster
-        table.row().colspan(2).expandX().fillX();
-        table.add(health).expandX().fillX();
-        table.add(buyh).expandX().fillX();
-        //adds the row and separate table for exit button
-        table.row().colspan(2).expandX().fillX();
-        table.add(subTable);
-        subTable.pad(16);
-        subTable.row().fillX().expandX();
-
-        subTable.add(buttonExit).width(cw/3.0f);
 
 
 
 
 
 
-        /*TextButton buttonQuit = new TextButton("Quit", shopSkin);
-        table.add(buttonQuit);
-        table.row();*/
-
-        buttonExit.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                game.changeScreen(DeathMarch.APPLICATION);
-            }
-        });
-//
-//        newGame.addListener(new ChangeListener() {
-//            @Override
-//            public void changed(ChangeEvent event, Actor actor) {
-//                game.changeScreen(DeathMarch.APPLICATION);
-//            }
-//        });
-//
-//        preferences.addListener(new ChangeListener(){
-//            @Override
-//            public void changed(ChangeEvent event, Actor actor){
-//                game.changeScreen(DeathMarch.PREFERENCES);
-//            }
-
-//        });
-//        newGame2.addListener(new ChangeListener() {
-//            @Override
-//            public void changed(ChangeEvent event, Actor actor) {
-//                game.changeScreen(DeathMarch.APP2);
-//            }
-//        });
-
-
+    }
+    private void checkBoundary(Player player){
+        if(player.getX()<0)player.setX(0);
+        if(player.getX()>1280-120)player.setX(1280-120);
     }
 }
 
