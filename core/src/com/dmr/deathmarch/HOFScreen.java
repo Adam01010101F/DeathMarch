b78b33d1a6b8e380c5325ec79d29c6cb0a75ffa9
Adam U Flores <watsaname1999@gmail.com>
2018-10-28 16:58:41 -0700

    Added Gson to the Gradle Build

--- core/src/com/dmr/deathmarch/HOFScreen.java
+++ core/src/com/dmr/deathmarch/HOFScreen.java
@@ -2,18 +2,23 @@ package com.dmr.deathmarch;
 
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-
+import com.google.gson.*;
 
 public class HOFScreen implements Screen {
     final DeathMarch game;
     private OrthographicCamera camera;
-    public HOFScreen(final DeathMarch game){
+    public HOFScreen(final DeathMarch game, Player player){
         this.game = game;
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1280, 720);
 
+        Gson gson = new Gson();
+        try{
 
+        }catch(RuntimeException e){
+
+        }
     }
 
     @Override
