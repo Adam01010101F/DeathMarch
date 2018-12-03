ea91233c7bd1df93033abf6de15908f4f9643448
mtran0132 <mtran0132@gmail.com>
2018-12-02 16:51:33 -0800

    added map

--- android/assets/maps/ground.tsx
+++ android/assets/maps/ground.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="ground" tilewidth="16" tileheight="16" tilecount="480" columns="30">
+<tileset version="1.2" tiledversion="1.2.1" name="ground" tilewidth="16" tileheight="16" tilecount="480" columns="30">
  <image source="tileset2.png" width="480" height="256"/>
+ <tile id="103">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
 </tileset>
